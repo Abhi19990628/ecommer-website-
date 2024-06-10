afd329702e84afb21513c6f4028eaa6c0c194784
abhihek kumar <abhbiv5976@gmail.com>
2024-06-10 14:16:48 +0530

    updated application

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-   path('home/' , views.Home , name='home'), 
+   path('' , views.Home , name='home'), 
+   path('about/' , views.About , name='about'), 
+   path('login/' , views.login_user , name='login'), 
+   path('logout/' , views.logout_user , name='logout'),
+   path('register/', views.register_user, name= 'register'),
+   path('product/<int:pk>', views.product, name='product'),
+   path('category/<str:foo>', views.category, name='category'),
 ]
+
--- store/views.py
+++ store/views.py
@@ -1,6 +1,77 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render,redirect  
+from .models import Product , Category
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 def Home(request):
-    return render(request ,'home.html')
+    product=Product.objects.all()
+    return render(request ,'home.html' ,  {'product': product })
 
+def About(request):
+    return render(request ,'about.html' ,  {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been logged in!")
+                return redirect('home')
+            else:
+                messages.error(request, "There was an error, please try again.")
+                return redirect('login')
+        else:
+            messages.error(request, "Please fill out both fields.")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out. Thanks for coming.")
+    return redirect('home')
+
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# log in user
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Username Created - Please Fill Out Your User Info Below..."))
+			return redirect('home')
+		else:
+			messages.success(request, ("Whoops! There was a problem Registering, please try again..."))
+			return redirect('register') 
+	else:
+		return render(request, 'register.html', {'form':form})
+
+def product(request,pk):
+	product = Product.objects.get(id=pk)
+	return render(request, 'product.html', {'product':product})
+
+def category(request, foo):
+	# Replace Hyphens with Spaces
+	foo = foo.replace('-', ' ')
+	# Grab the category from the url
+	try:
+		# Look Up The Category
+		category = Category.objects.get(name=foo)
+		products = Product.objects.filter(category=category)
+		return render(request, 'category.html', {'products':products, 'category':category})
+	except:
+		messages.success(request, ("That Category Doesn't Exist..."))
+		return redirect('home')
